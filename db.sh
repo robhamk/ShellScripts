c58b943d72c72308a938b30e441b67c45aa3c7fe
robhamk <robhamk@yahoo.com.tw>
2014-08-18 19:00:45 +0800

    linux script files
    This project collect some useful shell script which used when
    developing Android app

---
+++ db.sh
@@ -0,0 +1,2 @@
+adb shell "run-as com.xuenn.app.cloudmessage chmod 666 /data/data/com.xuenn.app.cloudmessage/databases/pushmessage.db"
+adb pull /data/data/com.xuenn.app.cloudmessage/databases/pushmessage.db
